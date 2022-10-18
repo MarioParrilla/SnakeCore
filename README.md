d93ab023980442a206ede5cd2851691613f88f8f
MarioParrilla <marioparrilla2@gmail.com>
2022-10-18 01:54:29 +0100

    New Version in Maven and Readme modificated

---
+++ README.md
@@ -0,0 +1,18 @@
+# Snake Core Library
+
+
+### To Implement in your project with:
+>**Change {CurrentVersion} with the current version**
+##### Maven
+````
+<dependency>
+    <groupId>io.github.marioparrilla</groupId>
+    <artifactId>snake-core</artifactId>
+    <version>{CurrentVersion}</version>
+</dependency>
+````
+
+##### Gradle
+````
+implementation 'io.github.marioparrilla:snake-core:{CurrentVersion}'
+````
--- target/apidocs/package-search-index.js
+++ target/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.marioparrilla.snake"},{"l":"com.marioparrilla.snake.Annotations"},{"l":"com.marioparrilla.snake.Context"},{"l":"com.marioparrilla.snake.ObjectsToTest"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.marioparrilla.snake.Annotations"},{"l":"com.marioparrilla.snake.Context"}];updateSearchResults();
