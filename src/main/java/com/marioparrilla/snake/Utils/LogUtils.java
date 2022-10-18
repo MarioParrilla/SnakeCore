05419162b8e356cfaee8e9242db1d91ebfc45035
MarioParrilla <marioparrilla2@gmail.com>
2022-10-18 20:15:09 +0100

    New Version 0.1.2 (Log Update)

--- src/main/java/com/marioparrilla/snake/Utils/LogUtils.java
+++ src/main/java/com/marioparrilla/snake/Utils/LogUtils.java
@@ -15,4 +15,47 @@ public class LogUtils {
             LoggerFactory.getLogger(clazz).info(msg);
     }
 
+    /**
+     * Trace via warn channel
+     * @param msg The message to trace
+     * @param clazz The class from where is going to trace
+     * @param canShowTrace The boolean that allow to trace
+     */
+    public static void warn(String msg, Class<?> clazz, boolean canShowTrace) {
+        if(canShowTrace)
+            LoggerFactory.getLogger(clazz).warn(msg);
+    }
+
+    /**
+     * Trace via error channel
+     * @param msg The message to trace
+     * @param clazz The class from where is going to trace
+     * @param canShowTrace The boolean that allow to trace
+     */
+    public static void error(String msg, Class<?> clazz, boolean canShowTrace) {
+        if(canShowTrace)
+            LoggerFactory.getLogger(clazz).error(msg);
+    }
+
+    /**
+     * Trace via debug channel
+     * @param msg The message to trace
+     * @param clazz The class from where is going to trace
+     * @param canShowTrace The boolean that allow to trace
+     */
+    public static void debug(String msg, Class<?> clazz, boolean canShowTrace) {
+        if(canShowTrace)
+            LoggerFactory.getLogger(clazz).debug(msg);
+    }
+
+    /**
+     * Trace via trace channel
+     * @param msg The message to trace
+     * @param clazz The class from where is going to trace
+     * @param canShowTrace The boolean that allow to trace
+     */
+    public static void trace(String msg, Class<?> clazz, boolean canShowTrace) {
+        if(canShowTrace)
+            LoggerFactory.getLogger(clazz).trace(msg);
+    }
 }
